52b7f7e7e50db7f19010043b43199f51b5b59899
srisruthi1234 <srisruthi147@gmail.com>
2017-07-31 12:37:38 +0530

    Create 3.c

---
+++ 3.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+int main()
+{
+int a,b,c;
+scanf("%d",%d,%d",&n,&s,&ns);
+if((a>b)&&(a>c))
+{
+printf("a is greater ");
+}
+else if((b>a)&&(b>c))
+{
+printf("\n b is greater");
+}
+else
+{
+printf("\n c is greater");
+}
+return 0;
+}
